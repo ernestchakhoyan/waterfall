9e4f501bc1d2ad77c082a7c3900dfed5ededa728
ernest <echakhoyan@bluip.com>
2020-06-14 12:33:03 +0400

    Welcome screen styling

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,19 +1,35 @@
 import React from "react";
 import {
-    Button,
     StyleSheet,
     Text,
+    TouchableOpacity,
     View
 } from "react-native";
-import { sizes,  colors } from "../constants/vars";
+import {
+    colors,
+    sizes
+} from "../constants/vars";
 
 function WelcomeScreen({ navigation }) {
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Welcome to WaterFall Game!</Text>
+            <View style={styles.content}>
+                <Text style={styles.title}>Welcome to WaterFall Game!</Text>
+                <View>
+                    <Text>Logo</Text>
+                </View>
+            </View>
             <View style={styles.buttonContainer}>
-                <Button style={styles.button} title="Start a new game" onPress={() => navigation.navigate("Game")} />
-                <Button style={styles.button} title="See the rules" onPress={() => navigation.navigate("GameRules")} />
+                <TouchableOpacity onPress={() => navigation.navigate("Game")}>
+                    <Text style={styles.button}>
+                        Start a new game
+                    </Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => navigation.navigate("GameRules")}>
+                    <Text style={styles.button}>
+                        See the rules
+                    </Text>
+                </TouchableOpacity>
             </View>
         </View>
     );
@@ -27,21 +43,37 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         backgroundColor: colors.primarySoft,
     },
+    content: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: sizes.spacer
+    },
     title: {
         fontSize: sizes.titleFontSizeMd,
         marginVertical: sizes.spacer,
-        fontFamily: "roboto-regular"
+        fontFamily: "roboto-regular",
+        color: "#fff"
     },
     buttonContainer: {
         width: "100%",
         flexDirection: "column",
         paddingHorizontal: sizes.spacer,
-        marginTop: sizes.spacer * 2
+        marginTop: sizes.spacer * 2,
+        marginBottom: sizes.spacer * 2
     },
     button: {
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        textAlign: "center",
+        textTransform: "uppercase",
         backgroundColor: "#000",
         borderColor: "#000",
-        color: "#fff"
+        color: "#fff",
+        padding: sizes.spacer,
+        borderRadius: 20,
+        marginTop: sizes.spacer
     }
 });
 
